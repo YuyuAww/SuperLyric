8c136cd9a34d3153765a470dd0828dc2c7444474
HChenX <HChen_LXi@outlook.com>
2025-05-12 15:39:57 +0800

    feat: New Version to 2.0
    #### 更新日志：
    - 修复：切换波点音乐模式为车载蓝牙模式，可修复之前的所有问题
    - 适配：Poweramp (未知模式)
    - 适配：青盐音乐 (魅族状态栏歌词)
    - 适配：OPPO音乐 (车载蓝牙歌词-请打开车载蓝牙歌词功能开关)
    - 适配：RPlayer (车载蓝牙歌词)
    - 优化：模块代码
    - 优化：更新 README
    
    ##### Tip：安装模块后建议重启下系统

--- app/src/main/java/com/hchen/superlyric/hook/music/Qishui.java
+++ app/src/main/java/com/hchen/superlyric/hook/music/Qishui.java
@@ -18,52 +18,66 @@
  */
 package com.hchen.superlyric.hook.music;
 
-import static android.view.View.GONE;
-
-import android.view.View;
-import android.widget.TextView;
-
 import com.hchen.collect.Collect;
 import com.hchen.hooktool.hook.IHook;
 import com.hchen.superlyric.hook.BaseLyric;
+import com.hchen.superlyric.utils.DexKitUtils;
 
-import java.util.HashSet;
+import org.luckypray.dexkit.query.FindMethod;
+import org.luckypray.dexkit.query.matchers.MethodMatcher;
+import org.luckypray.dexkit.result.MethodData;
+
+import java.lang.reflect.Method;
+import java.util.Objects;
 
 /**
  * 汽水音乐
+ *
+ * @author 焕晨HChen
  */
 @Collect(targetPackage = "com.luna.music")
 public class Qishui extends BaseLyric {
-    private static final HashSet<String> mTargetView = new HashSet<>();
-
-    static {
-        mTargetView.add("LyricTextView");
-        mTargetView.add("MarqueeLastLineLyricTextView");
-    }
+    private static Object BLUETOOTH;
 
     @Override
     protected void init() {
-        hookMethod(TextView.class,
-            "setText",
-            CharSequence.class,
-            new IHook() {
-                @Override
-                public void after() {
-                    if (mTargetView.contains(thisObject().getClass().getSimpleName())) {
-                        sendLyric((String) getArg(0));
-                    }
-                }
-            }
-        );
+        openBluetoothA2dp();
+        getMediaMetadataCompatLyric();
 
-        hookConstructor("com.luna.biz.playing.lyric.floatinglyrics.view.FloatingLyricFrameLayout",
-            android.content.Context.class, android.util.AttributeSet.class, int.class, new IHook() {
-                @Override
-                public void after() {
-                    View view = (View) thisObject();
-                    view.setVisibility(GONE);
-                }
-            }
-        );
+        Class<?> blueToothLyricStatus = findClass("com.luna.biz.playing.lyric.bluetoothlyrics.BlueToothLyricStatus");
+        BLUETOOTH = getStaticField("com.luna.common.arch.device.OutputDeviceType", "BLUETOOTH");
+        MethodData methodData = DexKitUtils.getDexKitBridge().findMethod(FindMethod.create()
+            .matcher(MethodMatcher.create()
+                .paramTypes(null, blueToothLyricStatus, boolean.class, int.class, Object.class)
+            )
+        ).single();
+
+        try {
+            Method method = methodData.getMethodInstance(classLoader);
+            Class<?> clazz = method.getParameterTypes()[0];
+
+            findMethodPro(clazz)
+                .withParamCount(1)
+                .withReturnType(boolean.class)
+                .single()
+                .hook(
+                    new IHook() {
+                        @Override
+                        public void before() {
+                            Object N = getArg(0);
+                            if (N == null) {
+                                setResult(true);
+                            } else {
+                                Object type = callMethod(N, "getType");
+                                if (Objects.equals(type, BLUETOOTH)) {
+                                    setResult(true);
+                                }
+                            }
+                        }
+                    }
+                );
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
